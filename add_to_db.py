589cbd472799836b2d231c05da8c86689c1b604e
AlterFritz88 <burdin009@gmail.com>
2020-02-23 18:47:50 +0300

    Задание сделано

--- add_to_db.py
+++ add_to_db.py
@@ -1,18 +1,28 @@
 import json
-from app import Teacher, db
+from app import Teacher, db, Request, Booking
+#
+# with open("teachers.json", 'r') as f:
+#     teachers = json.load(f)
+#
+#
+# db.create_all()
+# for t in teachers:
+#     teacher = Teacher(id=t['id'],
+#                       name=t["name"],
+#                       about=t["about"],
+#                       rating=t["rating"],
+#                       picture=t['picture'],
+#                       price=t['price'],
+#                       goals=" ".join(t['goals']),
+#                       free=json.dumps(t['free']))
+#     db.session.add(teacher)
+#
+# db.session.commit()
 
-with open("teachers.json", 'r') as f:
-    teachers = json.load(f)
+req = db.session.query(Teacher).all()
 
-print(teachers)
-db.create_all()
-for t in teachers:
-    teacher = Teacher(name=t["name"], about=t["about"], rating=t["rating"], picture=t['picture'], )
-    db.session.add(teacher)
-
-db.session.commit()
-
-teachers = db.session.query(Teacher).all()
-
-for t in teachers:
-    print(t.name)
+for t in req:
+    print(t.name, t.booking)
+    if t.booking:
+        for b in t.booking:
+            print(b.name)
--- app.py
+++ app.py
@@ -1,15 +1,40 @@
 import json
 import random
 from flask import Flask, render_template, request
+from flask import abort
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, ForeignKey, Integer, String, Float
 from sqlalchemy.orm import relationship
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, RadioField
+from wtforms.validators import InputRequired, Length, DataRequired
 
 from data import goals, weekdays
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///teachers_app.db'
+app.secret_key = 'superkey'
 db = SQLAlchemy(app)
+
+
+class RequestForm(FlaskForm):
+    name = StringField("Вас зовут: ", [InputRequired()])
+    phone = StringField("Ваш телефон: ", [InputRequired()])
+    goal = RadioField('Какая цель занятий?', default='travel', choices=[("travel", "Для путешествий"),
+                                                      ("study", "Для школы"),
+                                                      ("work", "Для работы"),
+                                                      ("relocate", "Для переезда")])
+    time = RadioField('Сколько времени есть?', default="1-2", choices=[("1-2", "1-2 часа в&nbsp;неделю"),
+                                                      ("3-5", "3-5 часов в&nbsp;неделю"),
+                                                      ("5-7", "5-7 часов в&nbsp;неделю"),
+                                                      ("7-10", "7-10 часов в&nbsp;неделю")], )
+    submit = SubmitField('Найдите мне преподавателя')
+
+
+class BookingForm(FlaskForm):
+    name = StringField("Вас зовут: ", [InputRequired()])
+    phone = StringField("Ваш телефон: ", [InputRequired()])
+    submit = SubmitField('Записаться на пробный урок')
 
 
 class Teacher(db.Model):
@@ -19,9 +44,9 @@ class Teacher(db.Model):
     about = Column(String, nullable=False)
     rating = Column(Float, nullable=False)
     picture = Column(String, nullable=False)
-    #price = Column(Integer, nullable=False)
-    #goals = Column(String, nullable=False)
-    #free = Column(String, nullable=False)
+    price = Column(Integer, nullable=False)
+    goals = Column(String, nullable=False)
+    free = Column(String, nullable=False)
     booking = relationship("Booking", back_populates="teacher")
 
 
@@ -30,6 +55,8 @@ class Booking(db.Model):
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
     phone = Column(String, nullable=False)
+    weekday = Column(String, nullable=False)
+    time = Column(String, nullable=False)
     teacher_id = Column(Integer, ForeignKey("teachers.id"))
     teacher = relationship("Teacher", back_populates="booking")
 
@@ -43,44 +70,46 @@ class Request(db.Model):
     time = Column(String, nullable=False)
 
 
-with open("teachers.json", 'r') as f:
-    teachers = json.load(f)
-
-
 @app.route('/')
 def main_page():
+    teachers = db.session.query(Teacher).all()
     random.shuffle(teachers)
     return render_template("index.html", teachers=teachers[:6])
 
 
 @app.route('/goals/<goal>')
 def goals_render(goal: str):
-    teachers_by_goal = sorted([x for x in teachers if goal in x['goals']], key=lambda x: x['rating'], reverse=True)
-    return render_template("goal.html", goal=goals[goal], teachers=teachers_by_goal)
+    teachers = db.session.query(Teacher).filter(Teacher.goals.contains(goal)).order_by(Teacher.rating.desc())
+    return render_template("goal.html", goal=goals[goal], teachers=teachers)
 
 
 @app.route('/all_teachers')
 def all_teachers():
-    teachers_by_goal = sorted(teachers, key=lambda x: x['rating'], reverse=True)
-    return render_template("all_teachers.html", teachers=teachers_by_goal)
+    teachers = db.session.query(Teacher).order_by(Teacher.rating.desc())
+    return render_template("all_teachers.html", teachers=teachers)
 
 
 @app.route('/profiles/<id_teacher>')
 def profile(id_teacher: str):
-    teacher = [x for x in teachers if x['id'] == int(id_teacher)][0]
-    free_times = {'mon': [x for x, y in teacher['free']['mon'].items() if y == True],
-                  'tue': [x for x, y in teacher['free']['tue'].items() if y == True],
-                  'wed': [x for x, y in teacher['free']['wed'].items() if y == True],
-                  'thu': [x for x, y in teacher['free']['thu'].items() if y == True],
-                  'fri': [x for x, y in teacher['free']['fri'].items() if y == True],
-                  'sat': [x for x, y in teacher['free']['sat'].items() if y == True],
-                  'sun': [x for x, y in teacher['free']['sun'].items() if y == True]}
+    teacher = db.session.query(Teacher).get(int(id_teacher))
+    if not teacher:
+        abort(404)
+
+    teacher_free = json.loads(teacher.free)
+    free_times = {'mon': [x for x, y in teacher_free['mon'].items() if y == True],
+                  'tue': [x for x, y in teacher_free['tue'].items() if y == True],
+                  'wed': [x for x, y in teacher_free['wed'].items() if y == True],
+                  'thu': [x for x, y in teacher_free['thu'].items() if y == True],
+                  'fri': [x for x, y in teacher_free['fri'].items() if y == True],
+                  'sat': [x for x, y in teacher_free['sat'].items() if y == True],
+                  'sun': [x for x, y in teacher_free['sun'].items() if y == True]}
     return render_template("profile.html", teacher=teacher, goals=goals, free_times=free_times)
 
 
 @app.route('/request')
 def request_teacher():
-    return render_template("request.html")
+    form = RequestForm()
+    return render_template("request.html", form=form)
 
 
 @app.route('/request_done', methods=['POST', 'GET'])
@@ -89,47 +118,36 @@ def request_done():
     time = request.form.get("time")
     name = request.form.get("name")
     phone = request.form.get("phone")
-    try:
-        with open("request.json", 'r') as request_file:
-            requests = json.loads(request_file.read())
-    except FileNotFoundError:
-        requests = []
-    requests.append({"name": name,
-                   "phone": phone,
-                   "goal": goal,
-                   "time": time})
-    with open("request.json", 'w') as request_file:
-        json.dump(requests, request_file)
-    print(goal, time, name, phone)
+    req = Request(name=name, phone=phone, time=time, goal=goal)
+    db.session.add(req)
+    db.session.commit()
     return render_template("request_done.html", goal=goals[goal], time=time, name=name, phone=phone)
 
 
 @app.route('/booking/<id_teacher>/<weekday>/<time>')
 def booking(id_teacher, weekday, time):
-    teacher = [x for x in teachers if x['id'] == int(id_teacher)][0]
-    return render_template("booking.html", teacher=teacher, weekday=weekday, weekday_rus=weekdays[weekday], time=time+":00")
+    teacher = db.session.query(Teacher).get(int(id_teacher))
+    form = BookingForm()
+    return render_template("booking.html", teacher=teacher, weekday=weekday, weekday_rus=weekdays[weekday], time=time+":00", form=form)
 
 
 @app.route('/booking_done', methods=['POST', 'GET'])
 def booking_done():
-    name = request.form.get("clientName")
-    phone = request.form.get("clientPhone")
-    teacher_id = request.form.get("clientTeacher")
+    name = request.form.get("name")
+    phone = request.form.get("phone")
+    id_teacher = request.form.get("clientTeacher")
     weekday = request.form.get("clientWeekday")
     time = request.form.get("clientTime")
-    try:
-        with open("booking.json", 'r') as orders_file:
-            orders = json.loads(orders_file.read())
-    except FileNotFoundError:
-        orders = []
-    orders.append({"name": name,
-                   "phone": phone,
-                   "teacher_id": int(teacher_id),
-                   "weekday": weekday,
-                   "time": time})
-    with open("booking.json", 'w') as orders_file:
-        json.dump(orders, orders_file)
+    teacher = db.session.query(Teacher).get(int(id_teacher))
+    book = Booking(name=name, phone=phone, weekday=weekday, time=time, teacher_id=id_teacher, teacher=teacher)
+    db.session.add(book)
+    db.session.commit()
     return render_template("booking_done.html", weekday=weekdays[weekday], time=time, name=name, phone_number=phone)
+
+
+@app.errorhandler(404)
+def invalid_route(e):
+    return render_template("error.html")
 
 
 if __name__ == "__main__":
